848fdd0a6dd8120e939a048ca6fee4d3c16af5f2
MrRobot <olivier.portal@laplateforme.io>
2025-04-28 17:27:32 +0200

    feat: add a chat box area + send button + entry key and set text area to read only

--- src/UI/window.c
+++ src/UI/window.c
@@ -1,8 +1,29 @@
 #include <gtk/gtk.h>
 #include "window.h"
 
-void print_hello(GtkWidget *widget, gpointer data) {
-    g_print("Hello World\n");
+GtkWidget *chat_entry_global = NULL;
+GtkWidget *chat_area_global = NULL;
+
+void send_message(GtkWidget *widget, gpointer data) {
+    const gchar *text = gtk_editable_get_text(GTK_EDITABLE(chat_entry_global));
+
+    if (text && *text != '\0') {
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(chat_area_global));
+        
+        GtkTextIter end;
+        gtk_text_buffer_get_end_iter(buffer, &end);
+
+        // Ajoute texte + saut de ligne
+        gtk_text_buffer_insert(buffer, &end, text, -1);
+        gtk_text_buffer_insert(buffer, &end, "\n", -1);
+
+        // Efface l'entrée
+        gtk_editable_set_text(GTK_EDITABLE(chat_entry_global), "");
+
+        // Scroll tout en bas
+        GtkTextMark *mark = gtk_text_buffer_get_insert(buffer);
+        gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(chat_area_global), mark);
+    }
 }
 
 void apply_custom_theme() {
@@ -59,7 +80,11 @@ void on_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *main_box;
     GtkWidget *channelSidebar;
+    GtkWidget *chat_box;
     GtkWidget *chat_area;
+    GtkWidget *chat_entry;
+    GtkWidget *chat_entry_box;
+    GtkWidget *chat_button;
     GtkWidget *userSidebar;
     GtkWidget *paned;
 
@@ -88,11 +113,45 @@ void on_activate(GtkApplication *app, gpointer user_data) {
     gtk_box_append(GTK_BOX(channelSidebar), label1);
     gtk_box_append(GTK_BOX(channelSidebar), label2);
 
-    // Main chat area
+    // Create the chat_box (vertical layout)
+    chat_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_widget_set_hexpand(chat_box, TRUE);
+    gtk_widget_set_vexpand(chat_box, TRUE);
+
+    // Create chat area (read only)
     chat_area = gtk_text_view_new();
     gtk_widget_set_name(chat_area, "chatview");
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(chat_area), FALSE); // read only
     gtk_widget_set_hexpand(chat_area, TRUE);
     gtk_widget_set_vexpand(chat_area, TRUE);
+
+    gtk_box_append(GTK_BOX(chat_box), chat_area);
+
+    // Create chat entry + send button (horizontal layout)
+    chat_entry_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_widget_set_name(chat_entry_box, "chatentrybox");
+
+    chat_entry = gtk_entry_new();
+    gtk_widget_set_name(chat_entry, "chatentry");
+    gtk_widget_set_hexpand(chat_entry, TRUE);
+
+    chat_entry_global = chat_entry;
+    chat_area_global = chat_area;    
+
+    chat_button = gtk_button_new_with_label("Send");
+    gtk_widget_set_name(chat_button, "chatbutton");
+    
+
+    gtk_box_append(GTK_BOX(chat_entry_box), chat_entry);
+    gtk_box_append(GTK_BOX(chat_entry_box), chat_button);
+
+    gtk_box_append(GTK_BOX(chat_box), chat_entry_box);
+
+    // Connect entry key
+    g_signal_connect(chat_entry, "activate", G_CALLBACK(send_message), NULL);
+
+    // Connect click on send button
+    g_signal_connect(chat_button, "clicked", G_CALLBACK(send_message), NULL);
 
     // Sidebar with users
     GtkWidget *userSidebarContent = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
@@ -112,7 +171,7 @@ void on_activate(GtkApplication *app, gpointer user_data) {
 
     // Paned between chat and users
     paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_paned_set_start_child(GTK_PANED(paned), chat_area);
+    gtk_paned_set_start_child(GTK_PANED(paned), chat_box);
     gtk_paned_set_end_child(GTK_PANED(paned), userSidebar);
     g_object_set_data(G_OBJECT(userSidebar), "paned", paned);
 
