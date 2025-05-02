bce588078f3b7ee207b02a9a9962534f34d377a4
MrRobot <olivier.portal@laplateforme.io>
2025-05-02 14:32:27 +0200

    refactor: update deparcated gtk 4 functions

--- src/UI/theme.c
+++ src/UI/theme.c
@@ -2,32 +2,46 @@
 #include "theme.h"
 
 void apply_custom_theme(GtkWidget *widget) {
-    GtkCssProvider *provider = gtk_css_provider_new();
+    if (!widget) {
+        g_printerr("❌ apply_custom_theme : widget NULL, impossible d'appliquer un thème.\n");
+        return;
+    }
+
     GdkDisplay *display = gtk_widget_get_display(widget);
+    if (!GDK_IS_DISPLAY(display)) {
+        g_printerr("❌ Erreur : affichage invalide (GDK_IS_DISPLAY failed).\n");
+        return;
+    }
 
-#ifdef _WIN32
-    gchar *theme_path = g_strdup("C:\\Users\\porta\\.themes\\MyBreeze-Dark-GTK\\gtk-4.0\\gtk.css");
-#else
-    gchar *theme_path = g_build_filename(g_get_home_dir(), ".themes", "MyBreeze-Dark-GTK", "gtk-4.0", "gtk.css", NULL);
-#endif
+    GtkCssProvider *provider = gtk_css_provider_new();
 
-// MyBreeze-Dark theme
-GFile *theme_file = g_file_new_for_path("C:/Users/porta/.themes/MyBreeze-Dark-GTK/gtk-4.0/gtk.css");
-gtk_css_provider_load_from_file(provider, theme_file);
-g_object_unref(theme_file);
+    // GTK Theme par défaut (MyBreeze-Dark)
+    const gchar *theme_path = "C:/Users/porta/.themes/MyBreeze-Dark-GTK/gtk-4.0/gtk.css";
+    GFile *theme_file = g_file_new_for_path(theme_path);
 
+    GError *error = NULL;
+    gtk_css_provider_load_from_file(provider, theme_file, &error);
+    g_object_unref(theme_file);
 
-// Custom css
-GtkCssProvider *custom = gtk_css_provider_new();
-gchar *css_path = g_build_filename(g_get_current_dir(), "custom.css", NULL);
-GFile *custom_file = g_file_new_for_path(css_path);
+    if (error) {
+        g_printerr("❌ Échec chargement du thème : %s\n", error->message);
+        g_clear_error(&error);
+    } else {
+        g_print("✅ Thème appliqué : %s\n", theme_path);
+        gtk_style_context_add_provider_for_display(display,
+            GTK_STYLE_PROVIDER(provider),
+            GTK_STYLE_PROVIDER_PRIORITY_THEME);
+    }
 
-gtk_css_provider_load_from_file(custom, custom_file);
-g_object_unref(custom_file);
+    // Ajout du custom.css optionnel (pas bloquant)
+    const gchar *custom_path = "./custom.css";
+    GFile *custom_file = g_file_new_for_path(custom_path);
+    GtkCssProvider *custom = gtk_css_provider_new();
 
-gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_THEME);
-gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER(custom), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    gtk_css_provider_load_from_file(custom, custom_file, NULL);
+    gtk_style_context_add_provider_for_display(display,
+        GTK_STYLE_PROVIDER(custom),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-g_free(css_path);
-g_free(theme_path);
+    g_object_unref(custom_file);
 }
